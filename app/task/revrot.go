693ee6b826146fdd60da3be39119e78c376a8dbd
Vladislav Gorokhov <jomira@tuta.io>
2024-11-28 12:16:47 +0800

    done: func Revrot

--- app/task/revrot.go
+++ app/task/revrot.go
@@ -1,9 +1,61 @@
 package task
 
+import (
+	"strconv"
+	"strings"
+)
+
+func reverseString(s string) string {
+	runes := []rune(s)
+
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+
+	return string(runes)
+}
+
+func splitStep(s string, step int) []string {
+	var result []string
+
+	for i := 0; i < len(s); i += step {
+		end := i + step
+		if end > len(s) {
+			end = len(s)
+		}
+		result = append(result, s[i:end])
+	}
+
+	return result
+}
+
+// O(n) == O(len(n))
 func Revrot(s string, n int) string {
 	if s == "" || n == 0 || len(s) < n {
 		return ""
 	}
 
-	return ""
+	sliceNumber := splitStep(s, n)
+	var resultList []string
+
+	for _, l := range sliceNumber {
+
+		if len(l) < n {
+			continue
+		}
+
+		sum := 0
+
+		for _, t := range l {
+			num, _ := strconv.Atoi(string(t))
+			sum += num
+		}
+		if sum%2 == 0 {
+			resultList = append(resultList, reverseString(l))
+		} else {
+			resultList = append(resultList, l[1:]+l[:1])
+		}
+	}
+
+	return strings.Join(resultList, "")
 }
--- tests/revrot_test.go
+++ tests/revrot_test.go
@@ -41,3 +41,30 @@ func TestRevrot_4(t *testing.T) {
 		t.Errorf("Revrot(\"1234\", 5) = %v; want %v", got, want)
 	}
 }
+
+func TestRevrot_5(t *testing.T) {
+	got := task.Revrot("66443875", 4)
+	want := "44668753"
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Revrot(\"66443875\", 4) = %v; want %v", got, want)
+	}
+}
+
+func TestRevrot_6(t *testing.T) {
+	got := task.Revrot("123456779", 8)
+	want := "23456771"
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Revrot(\"123456779\", 8) = %v; want %v", got, want)
+	}
+}
+
+func TestRevrot_7(t *testing.T) {
+	got := task.Revrot("123456987654", 6)
+	want := "234561876549"
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Revrot(\"123456987654\", 6) = %v; want %v", got, want)
+	}
+}
