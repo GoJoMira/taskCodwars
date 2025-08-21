34edd715bb3f6be2435933342228d2e50d69105a
vladislav gorokhoc <gvsnbala@gmail.com>
2025-08-21 08:43:59 +0800

    new test

--- app/task/maxProfit.go
+++ app/task/maxProfit.go
@@ -1,5 +1,39 @@
 package task
 
+// Task LeetCode MaxProfit
+
 func MaxProfit(prices []int) int {
-	return 0
+	var profit int
+	isActive := false
+	var assets int
+
+	for i, v := 0, 1; v < len(prices); i, v = i+1, v+1 {
+
+		now, end := prices[i], prices[v]
+
+		if !isActive {
+			if now < end {
+				assets = now
+				isActive = true
+			}
+		} else {
+			if v == len(prices)-1 && isActive {
+				if now > end {
+					profit += now - assets
+					continue
+				} else {
+					profit += end - assets
+				}
+			}
+
+			if now < end {
+				continue
+			} else {
+				profit += now - assets
+				isActive = false
+			}
+		}
+	}
+
+	return profit
 }
--- tests/maxProfit_test.go
+++ tests/maxProfit_test.go
@@ -13,3 +13,39 @@ func TestMaxProfit_1(t *testing.T) {
 		t.Errorf("MaxProfit = %d; want %d", got, want)
 	}
 }
+
+func TestMaxProfit_2(t *testing.T) {
+	got := task.MaxProfit([]int{7, 1, 5, 3, 6, 4})
+	want := 7
+
+	if got != want {
+		t.Errorf("MaxProfit = %d; want %d", got, want)
+	}
+}
+
+func TestMaxProfit_3(t *testing.T) {
+	got := task.MaxProfit([]int{7, 6, 4, 3, 1})
+	want := 0
+
+	if got != want {
+		t.Errorf("MaxProfit = %d; want %d", got, want)
+	}
+}
+
+func TestMaxProfit_4(t *testing.T) {
+	got := task.MaxProfit([]int{10, 15, 16, 5, 1, 5, 7})
+	want := 12
+
+	if got != want {
+		t.Errorf("MaxProfit = %d; want %d", got, want)
+	}
+}
+
+func TestMaxProfit_5(t *testing.T) {
+	got := task.MaxProfit([]int{1, 2})
+	want := 1
+
+	if got != want {
+		t.Errorf("MaxProfit = %d; want %d", got, want)
+	}
+}
