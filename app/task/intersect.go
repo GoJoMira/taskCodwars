8ab650358b228ebfa129a3b4ddf4a8febf6e1218
vladislav gorokhoc <gvsnbala@gmail.com>
2025-08-25 09:18:56 +0800

    my solution to the problem

--- app/task/intersect.go
+++ app/task/intersect.go
@@ -2,12 +2,15 @@ package task
 
 // https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/674/
 
-import "sort"
-
 func Intersect(nums1 []int, nums2 []int) []int {
 
 	if len(nums1) == 1 && len(nums2) == 1 {
-		return nums1
+
+		if nums1[0] == nums2[0] {
+			return nums1
+		}
+
+		return []int{}
 	}
 
 	var first, last []int
@@ -19,32 +22,28 @@ func Intersect(nums1 []int, nums2 []int) []int {
 		first, last = nums2, nums1
 	}
 
-	for _, v := range last {
-		for _, i := range first {
-			if v == i {
-				result = append(result, v)
+	indexFirstNumber := make(map[int]int)
+	indexlastNumber := make(map[int]int)
+
+	for i, v := range first {
+		for i2, v2 := range last {
+
+			if v == v2 {
+				_, ok := indexlastNumber[i2]
+				_, ok1 := indexFirstNumber[i]
+				if !ok && !ok1 {
+					indexFirstNumber[i] = 0
+					indexlastNumber[i2] = 0
+					result = append(result, v)
+				} else {
+					continue
+				}
 			}
+
 		}
 	}
 
-	var fainal_result []int
-
-	sortNums(result)
-
-	for i, j := 0, 1; j < len(result); i, j = i+1, j+1 {
-		if result[i] == result[j] {
-			fainal_result = append(fainal_result, result[i])
-		}
-	}
-
-	return fainal_result
+	return result
 }
 
-func sortNums(nums []int) {
-	sort.Slice(nums, func(i, j int) bool {
-		if nums[i] == nums[j] {
-			return nums[i] > nums[j]
-		}
-		return nums[i] < nums[j]
-	})
-}
+// Решение от LeetCode
--- tests/intersect_test.go
+++ tests/intersect_test.go
@@ -17,7 +17,7 @@ func TestIntersect_1(t *testing.T) {
 
 func TestIntersect_2(t *testing.T) {
 	got := task.Intersect([]int{4, 9, 5}, []int{9, 4, 9, 8, 4})
-	want := []int{4, 9}
+	want := []int{9, 4}
 
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("Intersect = %v; want %v", got, want)
@@ -25,7 +25,7 @@ func TestIntersect_2(t *testing.T) {
 }
 
 func TestIntersect_3(t *testing.T) {
-	got := task.Intersect([]int{4, 9, 6, 5}, []int{4, 6, 5, 6, 4, 9, 8, 9})
+	got := task.Intersect([]int{4, 5, 6, 9}, []int{4, 4, 5, 6, 6, 9, 8, 9})
 	want := []int{4, 5, 6, 9}
 
 	if !reflect.DeepEqual(got, want) {
@@ -36,6 +36,15 @@ func TestIntersect_3(t *testing.T) {
 func TestIntersect_4(t *testing.T) {
 	got := task.Intersect([]int{1, 2}, []int{1, 2, 2})
 	want := []int{1, 2}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Intersect = %v; want %v", got, want)
+	}
+}
+
+func TestIntersect_5(t *testing.T) {
+	got := task.Intersect([]int{3, 1, 2}, []int{1, 1})
+	want := []int{1}
 
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("Intersect = %v; want %v", got, want)
